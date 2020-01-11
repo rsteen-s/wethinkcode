9d1ed95aeb5ea21e5ea81ff0daeb09a21a048068
Rachel Steen - Stenersen <rachelsteen-stenersen@Rachels-MacBook-Pro.local>
2020-01-12 00:08:59 +0200

    this is function that runs the loop after recieving input from main

--- grouppro/rush.c
+++ grouppro/rush.c
@@ -1,55 +1,61 @@
 #include <unistd.h>
-#include <stdio.h>
+#include "ft_putchar.c"
 
 void rush(int x, int y)
 {
 	int x2;
 	int x3;
 	int x0;
-
+	char c;
+	
 	x0=x;
 	x0 = x0 - 2;
-
-	write(1,"o",1);
+	c = 'o';
+	ft_putchar(c);
 	
 	while(x0>0)
 	{
-		write(1,"-",1);
+		c = '-';
+		ft_putchar(c);
 		x0--;
 
 	}
-	write(1,"o\n",2);
+	c = 'o';
+	ft_putchar(c);
+	ft_putchar('\n');
 	
 	y = y - 2;
 	while(y>0)
 	{
-		write(1,"|",1);
+		c ='|';
+		ft_putchar(c);
 		x2=x;
 		x2=x2-2;
 			while(x2>0)
 			{ 
-				write(1," ",1);
+				c = ' ';	
+				ft_putchar(c);;
 				x2--;
 			}
 		
-		write(1,"|\n",2);
+		c = '|';
+		ft_putchar(c);
+		ft_putchar('\n');
 		y--;
 	}
-
-	write(1,"o",1);
+	c = 'o';
+	ft_putchar(c);
 	x3=x;
 	x3=x3-2;
 	while(x3>0)
 	{
-		write(1,"-",1);
+		c = '-';
+		ft_putchar(c);
 		x3--;
 	}
-	write(1,"o\n",2);
+	c = 'o';
+	ft_putchar(c);
+	ft_putchar('\n');
 
 }
 
-int main()
-{
-	rush(6,8);
-	return 0;
-}
