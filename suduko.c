14bc0a129e1793413650ac59ca79e7310cafaff7
Rachel Steen - Stenersen <rachelsteen-stenersen@Rachels-MacBook-Pro.local>
2020-01-19 17:24:17 +0200

    blaaaaah

--- suduko.c
+++ suduko.c
@@ -9,86 +9,96 @@
 /*   Updated: 2020/01/19 13:06:44 by rsteen-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int		 main (int x,char **board)
+int		 main (int argc, char **board)
 {
 	int	i;
+	int	y;
+	int	x;
 
-	x = x-1;
+	x = argc - 1;
 	while (x <= 9)
 	{
 		y = 0;
 		while (y <= 8)
 		{
-			if (board[x][y] != '.');
-			printf("%d", board[x][y]);
-			board[x][y] = //// Ascii + 0
+			if (board[x][y] != '.')
+			{
+				printf("%d", board[x][y]);
+				y++;
+			}
+
 
 			if (board[x][y] == '.')
-		
-			solvecell(board[x][y]);
-	y++;
- }*/
-x = 1;
-while (x <= 9)
-{
-	if (board[x][y] != '.')
-	printf("%d\n", board[x][y]);
+			{
+				i = 1;
+				num  = solvecell(board[x][y], i);
+				board[x][y] = num;
+				ft_putchar(num);
+				y++;
+			}
+		}
+		x++;
+	}
 
-
-	x++;
-
-//////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-
-
-int		solvecell(int	board[x][y], int i)
-
-while (i <= 9)
-{
-	board[x][y] = i;
-	if checkrow(board[x][y], i) == true
-		return (board[x][y] = i);
-	else 
-	i++;
-   solvecell(board[x][y], i)	
-
-
-	if checkcol(board[x][y]) == true
-		return (board[x][y] =i);
-	else 
-		i++;
-   solvecell(board[x][y], i)
-
-	if subboard(board[x][y] == true
-			return(board[x][y] = i);
-	else 
-	i++;
-	solvecell(board[x][y], i)
-
+	return (0);
 }
 
-return (board[x][y] = i);
+int		solvecell(int	board[x][y] ,int i)
+{
+	while (i <= 9)
+	{	
+		board[x][y] = i;
+		if (checkrow(board[x][y], i) == true)
 
-//////////////////////////////////////////
+			return (i);
+
+		else if
+			(checkrow(board[x][y], i) == false)
+			{ 
+				i++;
+				solvecell(board[x][y], i)
+			}
+
+
+		if (checkcol(board[x][y]) == true)
+			return (i);
+
+		else
+		{ 
+			i++;
+			solvecell(board[x][y], i);
+		}	
+
+		if (subboard(board[x][y]) == true)
+			return(i);
+
+		else
+		{ 
+			i++;
+			solvecell(board[x][y], i)
+		}	
+	}
+	return (i);
+}
 
 bool	checkrow(int	board[x][y], int i)
 {
 	int	counter;
-	
+
 	counter = 0;
 	while (board[x][counter] != '\0')
-{
-	if board[x][counter] == i;
-	counter++; 
+	{
+		if (board[x][counter] != i)
+			return true;
 
-	if board[x][counter] != i;
-	return true
-}
-return false
+
+		if (board[x][counter] == i)
+			return false;
+		counter++;
+	}
+
 }
 
-///////////////////////////////////////////
 
 bool		checkcol(int	board[x][y], int i)
 {
@@ -97,69 +107,77 @@ bool		checkcol(int	board[x][y], int i)
 	counter = 0;
 	while (board[counter][y] != '\0')
 	{
-		if board[counter][y] == i;
-		
-		return false 
+		if (board[counter][y] != i)
+			return true; 
 
-		if board[counter][y] != i;
+		if (board[counter][y] == i)
+			return false;
 		counter++;
-
 	}
-	return true
 
 }
-
-///////////////////////////////////////////////
 
 bool	subboard(int board[x][y],int i)
 {
 	int	k;
 	int l;
 
-	if (y >= 0) && (y <= 2)
-	   k = 0;
-	 
-	if (y > 2) && ( y <= 5)
+	if ((y >= 0) && (y <= 2))
+	{
+		k = 0;
+	}
+	else if ((y > 2) && ( y <= 5))
+	{
 		k = 3;
-		
-	if (y > 5) && (y <= 8)
-	    k = 6;
-	
-	if (x >= 1) && (x <= 3) 
+	}
+	else if ((y > 5) && (y <= 8))
+	{
+		k = 6;
+	}
+	else if ((x >= 1) && (x <= 3))
+	{
 		l = 0;
-	  
-	if ( x > 3) && (x <= 6)
+	}
+	else if (( x > 3) && (x <= 6))
+	{
 		l = 3;
+	}
+	else if ((x > 6) && (x <= 9))
+	{
+		l = 6;
+	}
+	else
 
-	if (x > 6) && (x <= 9)
-	    l = 6;
-
-///// y low = k and y high (k+3)
-//// x low = l and x high (l +3)
-
-	int	counterk;
+		int	counterk;
 	int	counterl;
 
-	counterk = k;
 	counterl = l;
 
-	while (counterl <= l+3)
+	while (counterl < l+3)
 	{
-		if board[counterl][k] == i;
-		return false
-
-		if board[counterl][k] != i;
-		counterl++;
-
-		while (counterk <= k +3)
+		counterk =k;
+		while (counterk < k + 3)
 		{
-			if board[counterl][counterk] == i;
-			return false	   	
-		
-			if board[counterl][counterk] != i;
+			if (board[counterl][counterk] == i)
+				return false;
+
+			else if (board[counterl][counterk] != i)
+				return true;
+			break;
+
 			counterk++;
 		}
-		return true
+
+		if (board[counterl][counterk] == i)
+		{
+			return false;
+
+		}
+		else if (board[counterl][counterk] != i)
+
+			counterl++;
+
 	}
-	return true
+	return true;
 }
+
